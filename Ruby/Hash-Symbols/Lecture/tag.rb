15e05cfa71c8e89e188b9333d64d71473411ecf0
daggerron <cmalfroy@student.42.fr>
2022-01-17 18:14:11 +0100

    "update"

--- Ruby/Hash-Symbols/Lecture/tag.rb
+++ Ruby/Hash-Symbols/Lecture/tag.rb
@@ -10,3 +10,27 @@ tag('a', "Trouver l'amour", {
 
 
 '<a href="http://tinder.com"> Trouver l\'amour </a>'
+
+
+# TODO: Convert the array of arrays into an array of hashes.
+students = [ [ "john", 28 ], [ "mary", 25 ], [ "paul", 21 ] ]
+students.map do |student|
+	{
+    name: student[0],
+    age: student[1]
+  }
+end
+
+"coucou #{first_name.capitalize} #{last_name.capitalize}"
+
+
+
+grades = [19, 8, 11, 15, 13]
+
+sum = 0
+
+grades.each do |grade|
+  sum += grade
+end
+
+sum / grades.length
